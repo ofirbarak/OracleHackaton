cb449558ea845533d2fc0f8ea10db2da53989a5c
ofirbarak <42671872+YanivBir@users.noreply.github.com>
2019-08-07 16:11:07 +0300

    finished HomePage

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,44 @@
 import React from 'react';
 import './App.css';
 import HomePage from './HomePage';
+import JoinGame from './JoinGame';
 
-function App() {
-  return (
-    <HomePage/>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentPage: 'HomePage'
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(key) {
+        return (value) => this.setState({ [key]: value });
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.currentPage == 'HomePage' ?
+                    <div>
+                        <div class="background-image"></div>
+                        <div class="bg-text">
+                            <HomePage 
+                                changePage={this.handleChange('currentPage')}
+                                changePlayerName={this.handleChange('playerName')}/>
+                        </div>
+                    </div> :
+                    null }
+                {this.state.currentPage == 'JoinGame' ?
+                    <div>
+                        <JoinGame />
+                    </div> :
+                    null }
+
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ client/src/JoinGame.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class JoinGame extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render(){
+        return (
+            <div>
+                asds
+            </div>
+        );
+    }
+}
+
+export default JoinGame;
