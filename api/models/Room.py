577a383aac1b8c8115d607c7fb3b867995db351b
avi <avrahamhaim2@gmail.com>
2019-08-07 22:58:13 +0300

    gitflow-feature: actions

--- api/models/Room.py
+++ api/models/Room.py
@@ -5,17 +5,15 @@ class Room:
     def __init__(self, creator):
         self.players = [creator]
         self.round = None
-        self.rules = [card_suit_rule]
+        self.rules = [self.card_suit_rule]
         self.name = creator.name + " room"
 
     def create(self, players):
         self.round = Round(players, self.rules)
 
+    def card_suit_rule(self, card, used_stack, player):
+        if card == 0:
+            return True, self.round_unchanged
+        return False, self.round_unchanged
 
-def card_suit_rule(card, used_stack, player):
-    if card == 0:
-        return True, round_unchanged
-    return False, round_unchanged
-
-
-def round_unchanged(round_instance): return round_instance
+    def round_unchanged(self, round_instance): return round_instance
